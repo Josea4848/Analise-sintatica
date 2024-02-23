30bf1822c3b38435a8ba308c6b62f6a8e2b45676
José Alves <josealves3416@gmail.com>
2024-02-23 08:20:12 -0300

    program

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-#tokens
-tokens = {"token":[], "tipo":[]}
+class Token:
+  def __init__(self, token, tipo) -> None:
+    self.token = token
+    self.tipo = tipo
+
+class Sintatico:
+  def __init__(self, listTokens):
+    self.tokens = listTokens
+    self.posicao = 0
+    self.erro = False
+    self.token = tokens[0]
+
+  def next(self):
+    self.posicao += 1
+    self.token = tokens[self.posicao]
+
+  def program(self):
+    if self.token.token == "program":
+      self.next()  
+      if self.token.tipo == "id":
+        self.next()
+        if self.token.token == ";":
+          print("program id;")
+        else:
+          self.erro = True     
+      else:
+        self.erro = True      
+    else:
+      self.erro = True     
+
+
 
 #Leitura dos tokens -> token | tipo
 tokensFile = open("tabela2.csv", "r")
 lines = tokensFile.readlines()
 lines.pop(0)
 
+#array de tokens
+tokens = list()
+
 #Adiciona tokens ao dicionário
 for line in lines:
-  line = line.replace('"',"").split(";")
-  tokens["token"].append(line[0])
-  tokens["tipo"].append(line[1])
+  line = line.split(" ")
+  tokens.append(Token(line[0], line[1]))
 
-for k,c  in tokens.items():
-  print(f"{k} {c}")
+
+for token in tokens:
+  print(f"{token.token} {token.tipo}")
+
+
+
+app = Sintatico(tokens)
+app.program()
